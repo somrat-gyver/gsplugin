d7fa672a1ff3093b7ea6afbf5b693f3458094d09
somrat <monnaf37@gmail.com>
2020-06-13 16:55:44 +0600

    added more sliders

--- js/script.js
+++ js/script.js
@@ -7,7 +7,57 @@
 		$('.wrapper').toggleClass('hide');
 	});
 
+	// popup video
+	var $videoSrc;
+	$('.video-btn').click(function () {
+		$videoSrc = $(this).data('src');
+	});
+	$('#popup-video').on('shown.bs.modal', function (e) {
+		$('#video').attr('src', $videoSrc + '?autoplay=1&amp;modestbranding=1&amp;showinfo=0');
+	});
+	$('#popup-video').on('hide.bs.modal', function (e) {
+		$('#video').attr('src', $videoSrc);
+	});
 
+	// page scroll
+	
+	var html_body = $('html, body');
+	$('.navbar-nav a, .page-scroll').on('click', function () { //use page-scroll class in any HTML tag for scrolling
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				html_body.animate({
+					scrollTop: target.offset().top - 0
+				}, 1500, 'easeInOutExpo');
+				return false;
+			}
+		}
+	});
+
+	// easeInOutExpo Declaration
+	jQuery.extend(jQuery.easing, {
+		easeInOutExpo: function (x, t, b, c, d) {
+			if (t === 0) {
+				return b;
+			}
+			if (t === d) {
+				return b + c;
+			}
+			if ((t /= d / 2) < 1) {
+				return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+			}
+			return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+		}
+	});
+
+	// tilt effect
+	$('.slider-tilt img').tilt({
+		maxTilt: 30,
+		scale: 1.1,
+		glare: true,
+		perspective: 200
+	});
 
 
 	// logo slider 1
@@ -39,7 +89,7 @@
 		]
 	});
 
-	
+
 	// logo slider 2
 	$('.logo-slider-2').slick({
 		infinite: true,
@@ -48,8 +98,8 @@
 		autoplay: true,
 		dots: false,
 		arrows: true,
-    prevArrow: '<button type=\'button\' class=\'arrow-prev\'><i class=\'ti-arrow-left\'></i></button>',
-    nextArrow: '<button type=\'button\' class=\'arrow-next\'><i class=\'ti-arrow-right\'></i></button>',
+		prevArrow: '<button type=\'button\' class=\'arrow-prev\'><i class=\'ti-arrow-left\'></i></button>',
+		nextArrow: '<button type=\'button\' class=\'arrow-next\'><i class=\'ti-arrow-right\'></i></button>',
 		responsive: [{
 				breakpoint: 1024,
 				settings: {
@@ -71,13 +121,180 @@
 		]
 	});
 
+	// logo slider 3
+	$('.logo-slider-3').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplay: true,
+		dots: true,
+		arrows: false,
+		customPaging: function (slider, i) {
+			var thumb = $(slider.$slides[i]).data('thumb');
+			return '<img src="' + thumb + '">';
+		},
+		responsive: [{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 4
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 2
+				}
+			}
+		]
+	});
 
+	// logo slider 4
+	$('.logo-slider-4').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplay: true,
+		dots: false,
+		arrows: false,
+		responsive: [{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 4
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 2
+				}
+			}
+		]
+	});
+
+	// logo slider 5
+	$('.logo-slider-5').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplay: true,
+		dots: false,
+		arrows: false,
+		centerMode: true,
+		responsive: [{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+
+
+	// logo slider 6
+	$('.logo-slider-6').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplay: true,
+		dots: false,
+		arrows: false,
+		responsive: [{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 4
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 2
+				}
+			}
+		]
+	});
 	
-	// customPaging: function (slider, i) {
-	// 	var icon = $(slider.$slides[i]).data('icon');
-	// 	var text = $(slider.$slides[i]).data('text');
-	// 	return '<a><i class="' + icon + '"></i><span>' + text + '</span></a>';
-	// }
 
+	// logo slider 7
+	$('.logo-slider-7').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplay: true,
+		dots: false,
+		arrows: false,
+		responsive: [{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 4
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 2
+				}
+			}
+		]
+	});
+	
+
+	// logo slider 8
+	$('.logo-slider-8').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplay: true,
+		dots: false,
+		arrows: false,
+		responsive: [{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 4
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 2
+				}
+			}
+		]
+	});
 
 })(jQuery);
